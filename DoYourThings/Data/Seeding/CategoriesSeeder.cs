b2d67eff5133c1d97bb3b21f8474d86688dba30c
Ivan Gyoshev <61377588+Ivan-Gyoshev@users.noreply.github.com>
2021-10-23 15:53:52 +0300

    Data seeding

---
+++ DoYourThings/Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,37 @@
+﻿namespace DoYourThings.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using DoYourThings.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Daily",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Weekly",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Monthly",
+            });
+        }
+    }
+}
--- DoYourThings/Startup.cs
+++ DoYourThings/Startup.cs
@@ -1,7 +1,8 @@
-namespace DoYourThings
+﻿namespace DoYourThings
 {
     using DoYourThings.Data;
     using DoYourThings.Data.Models;
+    using DoYourThings.Data.Seeding;
     using Microsoft.AspNetCore.Authentication;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -14,43 +15,39 @@ namespace DoYourThings
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
-            this.Configuration = configuration;
-        }
+            => this.Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     this.Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddDatabaseDeveloperPageExceptionFilter();
-
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-
             services.AddIdentityServer()
                 .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
             services.AddAuthentication()
                 .AddIdentityServerJwt();
-
             services.AddControllersWithViews();
             services.AddRazorPages();
-
-            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+                new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -59,17 +56,13 @@ namespace DoYourThings
             else
             {
                 app.UseExceptionHandler("/Error");
-
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseIdentityServer();
             app.UseAuthorization();
